4f8fd6bbd9cfbbdc3ffec38f6f39692dd14e28d9
Marcelo Carvalho <marcelomagal@gmail.com>
2023-09-21 22:23:14 -0300

    Implementado interface básica com Picker e DatePicker

--- GeradorDeEtiquetas/App.jsx
+++ GeradorDeEtiquetas/App.jsx
@@ -11,15 +11,32 @@ import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 const App = () => {
-  const [selectedCondition, setSelectedCondition] = useState('');
-  const [selectedFood, setSelectedFood] = useState('');
+  const [selectedCondition, setSelectedCondition] = useState(null);
+  const [selectedFood, setSelectedFood] = useState(null);
   const [date, setDate] = useState(new Date());
   const [showDatePicker, setShowDatePicker] = useState(false);
 
-  const onChangeDate = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    setShowDatePicker(false);
-    setDate(currentDate);
+  const foodValidity = {
+    Refrigerado: {
+      'Carne Bovina': 3,
+      'Peixe Fresco': 1,
+      'Frango': 2,
+    },
+    'Congelado -10°C a -18°C': {
+      'Carne Bovina': 120,
+      'Peixe Fresco': 60,
+      'Frango': 90,
+    },
+    'Congelado Temperatura menor que -18°C': {
+      'Carne Bovina': 365,
+      'Peixe Fresco': 180,
+      'Frango': 270,
+    },
+    'Seco (Após Abertura da Embalagem)': {
+      'Arroz': 180,
+      'Feijão': 180,
+      'Macarrão': 180,
+    },
   };
 
   return (
@@ -28,21 +45,26 @@ const App = () => {
 
       <Text>Condição de Armazenamento:</Text>
       <Picker
+        style={styles.picker}
         selectedValue={selectedCondition}
         onValueChange={(itemValue) => setSelectedCondition(itemValue)}
       >
-        <Picker.Item label="Refrigerado" value="Refrigerado" />
-        <Picker.Item label="Congelado -10°C a -18°C" value="Congelado -10°C a -18°C" />
-        <Picker.Item label="Congelado Temperatura menor que -18°C" value="Congelado Temperatura menor que -18°C" />
-        <Picker.Item label="Seco (Após Abertura da Embalagem)" value="Seco (Após Abertura da Embalagem)" />
+        <Picker.Item label="Selecione uma condição" value={null} />
+        {Object.keys(foodValidity).map((condition) => (
+          <Picker.Item key={condition} label={condition} value={condition} />
+        ))}
       </Picker>
 
       <Text>Alimento:</Text>
       <Picker
+        style={styles.picker}
         selectedValue={selectedFood}
         onValueChange={(itemValue) => setSelectedFood(itemValue)}
       >
-        {/* As opções serão preenchidas dinamicamente mais tarde */}
+        <Picker.Item label="Selecione um alimento" value={null} />
+        {selectedCondition && Object.keys(foodValidity[selectedCondition]).map((food) => (
+          <Picker.Item key={food} label={food} value={food} />
+        ))}
       </Picker>
 
       <Text>Data Inicial:</Text>
@@ -52,14 +74,27 @@ const App = () => {
           value={date}
           mode="date"
           display="default"
-          onChange={onChangeDate}
+          onChange={(event, selectedDate) => {
+            setShowDatePicker(false);
+            if (selectedDate) {
+              setDate(selectedDate);
+            }
+          }}
         />
       )}
 
       <Button title="Imprimir Etiqueta" onPress={() => {
-        // Aqui, vamos implementar a lógica para calcular a validade.
-      }} />
+        if (!selectedCondition || !selectedFood) {
+          alert('Por favor, selecione a condição de armazenamento e o alimento.');
+          return;
+        }
 
+        const validityDays = foodValidity[selectedCondition][selectedFood];
+        const expiryDate = new Date(date);
+        expiryDate.setDate(expiryDate.getDate() + validityDays);
+
+        alert(`A validade do ${selectedFood} é até ${expiryDate.toDateString()}`);
+      }} />
     </View>
   );
 };
@@ -70,6 +105,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#e0e0e0db',
+  },
+  picker: {
+    height: 50,
+    width: 250,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
   },
 });
 
